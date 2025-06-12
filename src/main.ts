1a62b6982079be64459090bbafdf1b97a64af984
marco-soria <marco.soriaa7@gmail.com>
2025-06-12 08:11:40 -0500

    added routing setup

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    console.log('Application bootstrapped successfully');
+  })
+  .catch((err) => console.error(err));
