b1016fa3f696df317f7ef1d20e356686ce8b5a4a
marco-soria <marco.soriaa7@gmail.com>
2025-06-12 15:16:49 -0500

    not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
-import { NoTaskComponent } from './tasks/no-task/no-task.component';
+
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -22,5 +24,9 @@ export const routes: Routes = [
         component: NewTaskComponent,
       },
     ],
+  },
+  {
+    path: '**', // <your-domain>/anything-else
+    component: NotFoundComponent,
   },
 ];
